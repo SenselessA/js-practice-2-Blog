92bffe9e530fe18db958217d9af6fdfeb6624983
Андрей <nekr000100@mail.ru>
2019-12-30 17:48:25 +0400

    full project for git pages

---
+++ src/services/transform.service.js
@@ -0,0 +1,9 @@
+export class TransformSerivice {
+    static fbObjectToArray(fbData) {
+        return Object.keys(fbData).map(key => {
+            const item = fbData[key]
+            item.id = key
+            return item
+        })
+    }
+}
